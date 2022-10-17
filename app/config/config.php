7701aaf62f477f3f2b1401c1dd30906aadc18be5
Laurin49 <welter@diwe.info>
2022-10-17 16:29:18 +0200

    Initial Setup for Chapter 05

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'Dirk');
+    define('DB_PASS', 'Welter56');
+    define('DB_NAME', 'sharedposts');
+
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost/shareposts');
+    // Site Name
+    define('SITENAME', 'SharePosts');
